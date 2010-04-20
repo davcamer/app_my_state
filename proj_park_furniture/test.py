fe694477a2cdbed27f1bb77ee0c560234279477d
Dave Cameron <dave.bc@gmail.com>
2010-04-20 23:05:47 +1000

    some cleanup. removing the ruby for the data. making labels work.

--- proj_park_furniture/test.py
+++ proj_park_furniture/test.py
@@ -57,24 +57,16 @@ def readData():
   for row in reader:
     easting, northing = row[EASTING_COLUMN], row[NORTHING_COLUMN]
     if easting == '' or northing == '':
-      print "discarding: ", row
+      print("discarding: ", row)
       discarded = discarded + 1
       continue
     lat, lon = ENtoLL84(easting, northing)
     writer.writerow(row + [lat, lon])
     
-    data.append({CATEGORY: row[CATEGORY_COLUMN], LATITUDE: lat, LONGITUDE: lon})
+    data.append({CATEGORY: row[CATEGORY_COLUMN], DESCRIPTION: row[DESCRIPTION_COLUMN], LATITUDE: lat, LONGITUDE: lon})
   
-  print "had to discard", discarded, "rows"
+  print("had to discard", discarded, "rows")
   return data
-
-def writeRubyOutput(data):
-  ruby = open('ruby_code.txt', 'w')
-  lineTemplate = string.Template("              {:description=>'$category',:latitude=>'$latitude',:longitude=>'$longitude'},\n")
-
-  for point in data:
-    ruby.write(lineTemplate.substitute(point))
-  ruby.close()
 
 def groupByCategory(items):
   groups = {}
@@ -85,7 +77,7 @@ def groupByCategory(items):
     groups[category].append(item)
   
   for key, value in groups.iteritems():
-    print len(value), key
+    print(len(value), key)
   
   return groups
 
@@ -93,7 +85,7 @@ def writeJavaScriptOutput(data):
   groups = groupByCategory(data)
   
   groupStartTemplate = string.Template("var $key = [\n")
-  lineTemplate = string.Template("  {category: '$category', latitude: '$latitude', longitude: '$longitude'},\n")
+  lineTemplate = string.Template("  {category: '$category', description: '$description', latitude: '$latitude', longitude: '$longitude'},\n")
   groupEnd = "];\n"
   
   js = open('data.js', 'w')
@@ -106,7 +98,6 @@ def writeJavaScriptOutput(data):
 
 def main():
   data = readData()
-  writeRubyOutput(data)
   writeJavaScriptOutput(data)
 
 if __name__ == '__main__':
